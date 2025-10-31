f321171e3a0b75fb378d1ca53e8b904e64932c48
ZlatoslavaVy <zlatoslava6b@gmail.com>
2025-10-31 17:37:44 +0200

    topic6_version2

--- topic6_task1.py
+++ topic6_task1.py
@@ -1,23 +1,21 @@
 def total_salary(path: str): 
-    with open(path, mode='w', encoding='utf-8') as file:
-        file.write("""Alex Korp,3000
-Nikita Borisenko,2000 
-Sitarama Raju,1000""" )
-        
     total = 0
-    developers = 0  
-    with open(path, mode='r', encoding='utf-8') as file:
+    developers = 0 
+    try: 
+        with open(path, mode='r', encoding='utf-8') as file:
 
-        for line in file:
-            clean_line = line.strip() 
-            clean_line = clean_line.split(',') 
-            print(clean_line)
-            salary = int(clean_line[1])
-            total += salary
-            developers += 1
-        print(developers)
-        average = total // developers
-        print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+            for line in file:
+                clean_line = line.strip() 
+                clean_line = clean_line.split(',') 
+                salary = int(clean_line[1])
+                total += salary
+                developers += 1
+            average = total // developers
+            print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+    except FileNotFoundError:
+        print("Помилка: Файл не знайдено.")
+    except UnicodeDecodeError:
+        print("Помилка: Проблема з кодуванням символів.")
     return total, average
 
 total_salary('salary_file.txt')
